f2cba95d5f5ca26119b02c6a86872fde3003fa3c
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2023-01-12 19:39:56 +0530

    Create H 105 : The Swap Function.c

---
+++ H 105 : The Swap Function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void swap(int, int);
+int main()
+{
+    int v1, v2;
+    scanf("%d %d", &v1, &v2);
+    swap(v1,v2);
+    printf("%d %d", v2, v1);
+    return 0;
+}
+//implement the function swap here
+void swap(int v1,int v2){
+    int temp = v1;
+    v1 = v2;
+    v2 = temp;
+}
